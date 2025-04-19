65210ce5427c9c0f0ba2e2e84eb4dc18e93ba3cd
yokohamacalcio <yokohamacalcio@gmail.com>
2025-04-20 00:02:44 +0900

    Create menu-toggle.js

---
+++ js/menu-toggle.js
@@ -0,0 +1,20 @@
+// yokohamacalcio/js/menu-toggle.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector('.mobile-menu-toggle');
+  const nav = document.querySelector('.main-nav');
+  const icon = toggleButton.querySelector('i');
+
+  toggleButton.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    
+    // Cambia icona da hamburger a X e viceversa
+    if (icon.classList.contains('fa-bars')) {
+      icon.classList.remove('fa-bars');
+      icon.classList.add('fa-times');
+    } else {
+      icon.classList.remove('fa-times');
+      icon.classList.add('fa-bars');
+    }
+  });
+});
